ddc95da7acb57351220148ef703f2eb6f6a58d44
Connor Rothschild <connorrothschild@gmail.com>
2021-05-10 06:54:16 -0500

    purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  purge: [],
+  purge: {
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
